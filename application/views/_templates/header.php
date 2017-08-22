c31e3a48c44b0c7a4d85b2cac3600db36212aaa6
cybersufi <cybersufi@gmail.com>
2017-08-22 21:35:22 +0700

    header template
    header template

--- application/views/_templates/header.php
+++ application/views/_templates/header.php
@@ -12,15 +12,15 @@
   <!-- Font Awesome -->
   <link rel="stylesheet" href="<?php echo base_url($frameworks_dir . '/font-awesome/css/font-awesome.min.css'); ?>">
   <!-- Ionicons -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+  <link rel="stylesheet" href="<?php echo base_url($frameworks_dir . '/ionicons/css/ionicons.min.css'); ?>">
   <!-- Theme style -->
-  <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
+  <link rel="stylesheet" href="<?php echo base_url($frameworks_dir . '/adminlte/css/AdminLTE.min.css'); ?>">
   <!-- AdminLTE Skins. We have chosen the skin-blue for this starter
         page. However, you can choose any other skin. Make sure you
         apply the skin class to the body tag so the changes take effect.
   -->
   
-  <link rel="stylesheet" href="dist/css/skins/skin-blue.min.css">
+  <link rel="stylesheet" href="<?php echo base_url($frameworks_dir . '/adminlte/css/skins/skin-red.min.css'); ?>">
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -30,33 +30,3 @@
   <![endif]-->
   <link rel="stylesheet" href="<?php echo base_url($frameworks_dir . '/adminlte/css/app.min.css'); ?>">
 </head>
-
-<?php /*-------------------------------------------------------------------------------------------------------------------------------------------*/ ?>
-<!doctype html>
-<html lang="<?php echo $lang; ?>">
-    <head>
-        <meta charset="<?php echo $charset; ?>">
-        <title><?php echo $title; ?></title>
-<?php if ($mobile === FALSE): ?>
-        <!--[if IE 8]>
-            <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <![endif]-->
-<?php endif; ?>
-        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-        <meta name="google" content="notranslate">
-        <meta name="robots" content="noindex, nofollow">
-        <link rel="icon" href="data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAqElEQVRYR+2WYQ6AIAiF8W7cq7oXd6v5I2eYAw2nbfivYq+vtwcUgB1EPPNbRBR4Tby2qivErYRvaEnPAdyB5AAi7gCwvSUeAA4iis/TkcKl1csBHu3HQXg7KgBUegVA7UW9AJKeA6znQKULoDcDkt46bahdHtZ1Por/54B2xmuz0uwA3wFfd0Y3gDTjhzvgANMdkGb8yAyY/ro1d4H2y7R1DuAOTHfgAn2CtjCe07uwAAAAAElFTkSuQmCC">
-        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,700italic">
-        <link rel="stylesheet" href="<?php echo base_url($frameworks_dir . '/bootstrap/css/bootstrap.min.css'); ?>">
-        <link rel="stylesheet" href="<?php echo base_url($frameworks_dir . '/font-awesome/css/font-awesome.min.css'); ?>">
-        <link rel="stylesheet" href="<?php echo base_url($frameworks_dir . '/adminlte/css/adminlte.min.css'); ?>">
-        <link rel="stylesheet" href="<?php echo base_url($plugins_dir . '/icheck/css/blue.css'); ?>">
-<?php if ($mobile === FALSE): ?>
-        <!--[if lt IE 9]>
-            <script src="<?php echo base_url($plugins_dir . '/html5shiv/html5shiv.min.js'); ?>"></script>
-            <script src="<?php echo base_url($plugins_dir . '/respond/respond.min.js'); ?>"></script>
-        <![endif]-->
-<?php endif; ?>
-    </head>
-    <body class="hold-transition login-page">
-        <div class="login-box">
