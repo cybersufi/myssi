30cdca8b941a38b5171d275448ba511e01edaac4
cybersufi <cybersufi@gmail.com>
2017-10-06 15:25:35 +0700

    ssss
    ssss

--- application/controllers/Usergroups.php
+++ application/controllers/Usergroups.php
@@ -42,8 +42,39 @@ class Usergroups extends MY_Controller
         if ($this->auth_lib->logged_in() )
         {
             $this->set_pagename('groupdetail');
-            $this->data['pagetitle'] = "Add New Groups";
-            $this->template->render('usergroups/groupdetail', $this->data);
+            $this->data['pagetitle'] = "Add New Group";
+
+            if ($this->form_validation->run() == TRUE)
+            {
+                return false;
+            }
+            else
+            {
+                $this->data['message'] = $this->session->flashdata('message');
+                $this->data['error'] = (validation_errors()) ? validation_errors() : $this->session->flashdata('error');
+
+                $this->data['name'] = array(
+                    'name'        => 'name',
+                    'id'          => 'name',
+                    'type'        => 'text',
+                    'value'       => $this->form_validation->set_value('name'),
+                    'class'       => 'form-control',
+                    'placeholder' => 'Group Name'
+                );
+
+                $this->data['description'] = array(
+                    'name'        => 'description',
+                    'id'          => 'description',
+                    'value'       => $this->form_validation->set_value('description'),
+                    'class'       => 'form-control',
+                    'placeholder' => 'Group description'
+                );
+
+                
+
+                /* Load Template */
+                $this->template->render('usergroups/detail', $this->data);
+            }
         }
         else 
         {
